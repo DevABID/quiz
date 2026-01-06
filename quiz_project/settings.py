554c9594473ff802a1428f3538c109be0e0b747b
DevABID <92073265+DevABID@users.noreply.github.com>
2026-01-06 19:14:23 +0600

    Update settings.py

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -4,7 +4,8 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-this-with-a-secure-secret-for-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [    "quiz-y9g8.onrender.com",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
